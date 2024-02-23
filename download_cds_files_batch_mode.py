d4dd3e3d527696429d1ea11bcadf59966fd61305
Rishabh Gupta <gupt1075@gilbreth-k006.rcac.purdue.edu>
2024-02-22 20:36:06 -0500

    downloading 10 GiB file from netcdf from CDS

--- download_cds_files_batch_mode.py
+++ download_cds_files_batch_mode.py
@@ -11,6 +11,7 @@ start_time = datetime(2020, 3, 1, 0, 0, 0)
 end_time = datetime(2023, 9, 1, 23, 59, 59)
 timedelta = timedelta(hours=6)
 current_time = start_time
+
 timestamps = []
 while(current_time <= end_time):
     timestamps.append(current_time)
@@ -38,4 +39,36 @@ c.retrieve('reanalysis-era5-complete', {
     'area'    : 'global',              # Geographical subarea: North/West/South/East
     'grid'    : '0.25/0.25',                   # Grid resolution: latitude/longitude
     'format'  : 'netcdf',                    # Output format, requires 'grid' to be specified
-}, f"{output_path}/ERA5-pl-z500.25.nc")        # Output filename
+}, f"{output_path}" + f"_{start_time.strftime('%Y-%m-%d')}_to_{end_time.strftime('%Y-%m-%d')}_" + "/ERA5-pl-z500.25.nc")        # Output filename
+
+
+
+
+
+
+# module load netcdf
+# $ ncdump -c ERA5-pl-z500.25.nc
+# netcdf ERA5-pl-z500.25 {
+# dimensions:
+# 	longitude = 1440 ;
+# 	latitude = 721 ;
+# 	time = 16 ;
+# variables:
+# 	float longitude(longitude) ;
+# 		longitude:units = "degrees_east" ;
+# 		longitude:long_name = "longitude" ;
+# 	float latitude(latitude) ;
+# 		latitude:units = "degrees_north" ;
+# 		latitude:long_name = "latitude" ;
+# 	int time(time) ;
+# 		time:units = "hours since 1900-01-01 00:00:00.0" ;
+# 		time:long_name = "time" ;
+# 		time:calendar = "gregorian" ;
+# 	short z(time, latitude, longitude) ;
+# 		z:scale_factor = 0.167655930504479 ;
+# 		z:add_offset = 52726.9220314097 ;
+# 		z:_FillValue = -32767s ;
+# 		z:missing_value = -32767s ;
+# 		z:units = "m**2 s**-2" ;
+# 		z:long_name = "Geopotential" ;
+# 		z:standard_name = "geopotential" ;
